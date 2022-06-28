71981498830caee485aa469c33e2ac3e3940c82f
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 01:36:34 +0530

    Add project files.

---
+++ interface simple/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace interface_simple
+{
+    interface simple
+    {  
+        void S(int num);
+        public abstract int show(int num1,int num2);
+        public abstract void so();
+
+     /*  douat   public static int k; */  
+    }
+   
+    class Program:simple
+    {
+       
+        public void S(int n)
+        {
+            Console.WriteLine("first interface  :"+n);
+
+        }
+        public int show(int num1,int num2)
+        {
+            return num1+num2 ;
+        }
+        public  void so()
+        {
+            Console.WriteLine("hello :");
+        }
+      
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.S(6);
+           int result= obj.show(12, 101);
+            Console.WriteLine(" Abstract method with parametar with return type :"+result);
+            obj.so();
+            
+           
+           
+           
+            
+        }
+    }
+}
